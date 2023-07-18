1d8f8a875482131d76af6ff6cb856d2554315694
FiloCara <fili.cara@gmail.com>
2023-07-18 16:30:10 -0400

    Update examples and Readme

--- Readme.md
+++ Readme.md
@@ -15,29 +15,32 @@ Key Features:
 ## Example
 
 ```python
-from pyS7 import Client, Item, MemoryArea, DataType
+from pyS7 import Client
 
 if __name__ == "__main__":
 
-    client = Client(address="localhost", rack=0, slot=1)
+    # Create a new Client object to connect to S7-300/400/1200/1500 PLC.
+    # Provide the PLC's IP address and slot/rack information
+    client = Client(address="192.168.5.100", rack=0, slot=1)
 
+    # Establish connection with the PLC
     client.connect()
 
-    item0 = Item(MemoryArea.DB, 1, DataType.INT, 34, 0, 1)
-    item1 = Item(MemoryArea.DB, 1, DataType.INT, 30, 0, 1)
-    item2 = Item(MemoryArea.DB, 1, DataType.INT, 32, 0, 1)
-    item3 = Item(MemoryArea.DB, 1, DataType.INT, 38, 0, 1)
-    item4 = Item(MemoryArea.DB, 1, DataType.INT, 36, 0, 1)
-    item5 = Item(MemoryArea.DB, 1, DataType.REAL, 64, 0, 1)
-    item6 = Item(MemoryArea.DB, 1, DataType.REAL, 72, 0, 4)
-    item7 = Item(MemoryArea.DB, 1, DataType.REAL, 68, 0, 1)
-
-
+    # Define area tags to read
     items = [
-        item0, item1, item2, item3, 
-        item4, item5, item6, item7
+        "DB1,X0.0",     # Read BIT 0 (first bit) of DB1
+        "DB1,X0.6",     # Read BIT 7 (7th bit) of DB1
+        "DB1,I30",      # Read INT at address 30 of DB1
+        "M54.4",        # Read BIT 4 (fifth bit) in the merker (memento) area
+        "IW22",         # Read WORD at address 22 in input area
+        "QR,24",        # Read REAL at address 24 in output area
+        "DB1,S10.5"     # Read sequence of CHAR of length 5 starting at address 10 of DB1
     ]
 
+    # Read the data from the PLC using the specified items list
     data = client.read(items=items)
-    print(data)
+
+    print(data)  # [True, False, 123, True, 10, -2.54943805634653e-12, 'Hello']
 ```
+
+Look at the examples folder for more self-explanatory examples. 
--- examples/read_data.py
+++ examples/read_data.py
@@ -1,32 +1,26 @@
-from pyS7 import Client, Item, MemoryArea, DataType
+from pyS7 import Client
 
 if __name__ == "__main__":
 
-    client = Client(address="localhost", rack=0, slot=1)
+    # Create a new Client object to connect to S7-300/400/1200/1500 PLC.
+    # Provide the PLC's IP address and slot/rack information
+    client = Client(address="192.168.5.100", rack=0, slot=1)
+
+    # Establish connection with the PLC
     client.connect()
 
-    item0 = Item(MemoryArea.DB, 1, DataType.INT, 34, 0, 1)
-    item1 = Item(MemoryArea.DB, 1, DataType.INT, 30, 0, 1)
-    item2 = Item(MemoryArea.DB, 1, DataType.INT, 32, 0, 1)
-    item3 = Item(MemoryArea.DB, 1, DataType.INT, 38, 0, 1)
-    item4 = Item(MemoryArea.DB, 1, DataType.INT, 36, 0, 1)
-    item5 = Item(MemoryArea.DB, 1, DataType.REAL, 64, 0, 1)
-    item6 = Item(MemoryArea.DB, 1, DataType.REAL, 72, 0, 4)
-    item7 = Item(MemoryArea.DB, 1, DataType.REAL, 68, 0, 1)
-
-
+    # Define area tags to read
     items = [
-        item0,
-        item1,
-        item2,
-        item3,
-        item4,
-        item5,
-        item6,
-        item7,
+        "DB1,X0.0",     # => Item(MemoryArea.DB, 1, DataType.BIT, 0, 0, 1) - Read BIT 0 (first bit) of DB1
+        "DB1,X0.6",     # => Item(MemoryArea.DB, 1, DataType.BIT, 0, 6, 1) - Read BIT 7 (7th bit) of DB1
+        "DB1,I30",      # => Item(MemoryArea.DB, 1, DataType.INT, 30, 0, 1) - Read INT at address 30 of DB1
+        "M54.4",        # => Item(MemoryArea.MERKER, 0, DataType.BIT, 4, 4, 1) - Read BIT 4 (fifth bit) in the merker (memento) area
+        "IW22",         # => Item(MemoryArea.INPUT, 0, DataType.WORD, 22, 0, 1) - Read WORD at address 22 in input area
+        "QR,24",        # => Item(MemoryArea.OUTPUT, 0, DataType.REAL, 24, 0, 1) - Read REAL at address 24 in output area
+        "DB1,S10.5"     # => Item(MemoryArea.DB, 1, DataType.CHAR, 10, 0, 5) - Read sequence of CHAR of length 5 starting at address 10 of DB1
     ]
 
-    data = client.read(items=items, optimize=True)
-    print(data) # [0, -32768, -1234, 32767, 1234, -3402823106560.0, (-1.1754943806535634e-12, 0.0, 1.1754943508222875e-38, 1.1754943806535634e-12), -1.7549434765121066e-30]
+    # Read the data from the PLC using the specified items list
+    data = client.read(items=items)
 
-
+    print(data)  # [True, False, 123, True, 10, -2.54943805634653e-12, 'Hello']
