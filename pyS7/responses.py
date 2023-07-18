c0fd4f1df7a2a0e7cf668d14962c0bc23ede6057
FiloCara <fili.cara@gmail.com>
2023-07-18 15:35:36 -0400

    Add support for CHAR Item

--- pyS7/responses.py
+++ pyS7/responses.py
@@ -30,9 +30,9 @@ class PDUNegotiationResponse:
         return (max_jobs_calling, max_jobs_called, pdu_size)
 
 
-def parse_read_response_optimized(bytes_response: bytes, item_map: ItemsMap) -> list[bool | int | float | tuple[bool | int | float, ...]]:
+def parse_read_response_optimized(bytes_response: bytes, item_map: ItemsMap) -> list[bool | int | float | str | tuple[bool | int | float, ...]]:
 
-    parsed_data: list[tuple[int, tuple[bool | int | float, ...]]] = []
+    parsed_data: list[tuple[int, tuple[bool | int | float | str, ...]]] = []
     offset: int = 21  # Response offset where data starts
 
     for packed_item in item_map.keys():
@@ -53,6 +53,11 @@ def parse_read_response_optimized(bytes_response: bytes, item_map: ItemsMap) -> 
                 elif item.data_type == DataType.BYTE:
                     data = struct.unpack_from(
                         f">{(item.start - packed_item.start) * 'x'}{item.length * 'B'}", bytes_response, offset)
+                
+                elif item.data_type == DataType.CHAR:
+                    str_start = offset + (item.start - packed_item.start)
+                    str_end = offset + (item.start - packed_item.start) + item.length
+                    data = bytes_response[str_start:str_end].decode()
 
                 elif item.data_type == DataType.INT:
                     data = struct.unpack_from(
@@ -73,6 +78,9 @@ def parse_read_response_optimized(bytes_response: bytes, item_map: ItemsMap) -> 
                 elif item.data_type == DataType.REAL:
                     data = struct.unpack_from(
                         f">{(item.start - packed_item.start) * 'x'}{item.length * 'f'}", bytes_response, offset)
+                
+                else:
+                    raise ValueError(f"DataType: {item.data_type} not supported")
 
                 parsed_data.append((idx, data))
 
@@ -85,12 +93,12 @@ def parse_read_response_optimized(bytes_response: bytes, item_map: ItemsMap) -> 
 
     parsed_data.sort(key=lambda elem: elem[0])
 
-    processed_data: list[bool | int | float | tuple[bool | int | float, ...]] = [data[0] if isinstance(data, tuple) and len(
+    processed_data: list[bool | int | float | str | tuple[bool | int | float, ...]] = [data[0] if isinstance(data, tuple) and len(
         data) == 1 else data for (_, data) in parsed_data]
     return processed_data
 
 
-def parse_read_response(bytes_response: bytes, items: list[Item]) -> list[bool | int | float | tuple[bool | int | float, ...]]:
+def parse_read_response(bytes_response: bytes, items: list[Item]) -> list[bool | int | float | str | tuple[bool | int | float, ...]]:
 
     parsed_data: list[tuple[bool | int | float, ...]] = []
     offset = 21  # Response offset where data starts
@@ -112,6 +120,10 @@ def parse_read_response(bytes_response: bytes, items: list[Item]) -> list[bool |
                 data = struct.unpack_from(
                     f">{item.length * 'B'}", bytes_response, offset)
                 offset += item.size()
+            
+            elif item.data_type == DataType.CHAR:
+                data = bytes_response[offset:offset + item.length].decode()
+                offset += item.size()  
 
             elif item.data_type == DataType.INT:
                 data = struct.unpack_from(
@@ -147,7 +159,7 @@ def parse_read_response(bytes_response: bytes, items: list[Item]) -> list[bool |
             raise Exception(
                 f"Impossible to parse response data from item {item}")
 
-    processed_data: list[bool | int | float | tuple[bool | int | float, ...]] = [data[0] if isinstance(data, tuple) and len(
+    processed_data: list[bool | int | float | str | tuple[bool | int | float, ...]] = [data[0] if isinstance(data, tuple) and len(
         data) == 1 else data for data in parsed_data]
 
     return processed_data
@@ -159,7 +171,7 @@ class ReadResponse:
         self.response = response
         self.items = items
 
-    def parse(self) -> list[bool | int | float | tuple[bool | int | float, ...]]:
+    def parse(self) -> list[bool | int | float | str | tuple[bool | int | float, ...]]:
         return parse_read_response(bytes_response=self.response, items=self.items)
 
 
@@ -169,7 +181,7 @@ class ReadOptimizedResponse:
         self.response = response
         self.items_map = items_map
 
-    def parse(self) -> list[bool | int | float | tuple[bool | int | float, ...]]:
+    def parse(self) -> list[bool | int | float | str | tuple[bool | int | float, ...]]:
         return parse_read_response_optimized(bytes_response=self.response, item_map=self.items_map)
 
 
