b88011839382920d54fba0d7a78946f897b0bb41
FiloCara <fili.cara@gmail.com>
2023-07-21 15:46:58 -0400

    Add new example

---
+++ examples/read_data_no_client.py
@@ -0,0 +1,68 @@
+"""
+#######################################################################
+# The following code is meant for demonstration purposes only.
+# In a real-world application, you should use the Client class, 
+# which provides a high-level interface to interact
+# with S7 devices and ensures proper handling of resources.
+#######################################################################
+"""
+
+import socket
+
+from pyS7 import (Item, PDUNegotiationRequest, PDUNegotiationResponse,
+                  ReadRequest, ReadResponse, map_address_to_item)
+
+if __name__ == "__main__":
+
+    address: str = "192.168.5.100"
+    port: int = 102
+
+    addresses = [
+        "DB1,X0.0",
+        "DB1,X0.6",
+        "DB1,I30",
+        "M54.4",
+        "IW22",
+        "QR24",
+        "DB1,S10.5"
+    ]
+
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    #####################################################
+    ############# ESTABLISH CONNECTION ##################
+    #####################################################
+
+    # Establish TCP connection
+    socket.connect((address, port))
+
+    # Setup Communication -> request
+    negotiation_request = PDUNegotiationRequest(max_pdu=960)
+    socket.send(negotiation_request.serialize())
+
+    # Setup Communication -> ack
+    negotiation_bytes_response: bytes = socket.recv(980)
+    negotiation_response = PDUNegotiationResponse(
+        response=negotiation_bytes_response)
+
+    # Extract negotiated pdu...
+    max_jobs_calling, max_jobs_called, pdu_size = negotiation_response.parse()
+
+    #####################################################
+    ################# READ REQUEST ######################
+    #####################################################
+
+    # Make sure to convert string addresses to Items
+    items: list[Item] = [map_address_to_item(addr) for addr in addresses]
+
+    # Read
+    read_request = ReadRequest(items=items)
+    socket.send(read_request.serialize())
+
+    # Read -> ack
+    read_bytes_response: bytes = socket.recv(pdu_size)
+    read_response = ReadResponse(response=read_bytes_response, items=items)
+
+    data = read_response.parse()
+
+    print(data)  # [True, False, 123, True, 10, -2.54943805634653e-12, 'Hello']
