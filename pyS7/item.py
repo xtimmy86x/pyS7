e8fab2e1126cb23bd49bc27597124ab573a9c314
FiloCara <fili.cara@gmail.com>
2023-07-31 14:42:40 -0400

    Enforce type and value checking in Item post_init

--- pyS7/item.py
+++ pyS7/item.py
@@ -14,34 +14,40 @@ class Item:
 
     def __post_init__(self) -> None:
         if not isinstance(self.memory_area, MemoryArea):
-            raise TypeError("memory_area should be of type MemoryArea")
+            raise TypeError("Invalid memory_area: Expected type MemoryArea, got " + str(type(self.memory_area)))
 
         if not isinstance(self.db_number, int):
-            raise TypeError("db_number should be of type int")
+            raise TypeError("Invalid db_number: Expected type int, got " + str(type(self.db_number)))
+
+        if self.memory_area != MemoryArea.DB and self.db_number > 0:
+            raise ValueError(f"Invalid db_number: Must be 0 when memory_area is {self.memory_area}, but got {self.db_number}")
         
         if self.db_number < 0:
-            raise ValueError("db_number value must be greater then 0")
+            raise ValueError(f"Invalid db_number: Expected non-negative value, got {self.db_number}")
 
         if not isinstance(self.data_type, DataType):
-            raise TypeError("data_type should be of type DataType")
+            raise TypeError("Invalid data_type: Expected type DataType, got " + str(type(self.data_type)))
 
         if not isinstance(self.start, int):
-            raise TypeError("start should be of type int")
+            raise TypeError("Invalid start: Expected type int, got " + str(type(self.start)))
         
         if self.start < 0:
-            raise ValueError("start value must be greater or equal to 0")
+            raise ValueError(f"Invalid start: Expected non-negative value, got {self.start}")
 
         if not isinstance(self.bit_offset, int):
-            raise TypeError("bit_offset should be of type int")
+            raise TypeError("Invalid bit_offset: Expected type int, got " + str(type(self.bit_offset)))
+        
+        if self.data_type != DataType.BIT and self.bit_offset > 0:
+            raise ValueError(f"Invalid bit_offset: Must be 0 when data_type is not DataType.BIT, but got {self.bit_offset}")
 
         if self.bit_offset < 0 or self.bit_offset > 7:
-            raise ValueError("bit_offset value must be between 0 and 7")
+            raise ValueError(f"Invalid bit_offset: Expected value between 0 and 7, got {self.bit_offset}")
 
         if not isinstance(self.length, int):
-            raise TypeError("length should be of type int")
+            raise TypeError("Invalid length: Expected type int, got " + str(type(self.length)))
 
         if self.length <= 0:
-            raise ValueError("length value must be greaten than 0")
+            raise ValueError(f"Invalid length: Expected positive value, got {self.length}")
 
     def size(self) -> int:
         """Return the Item size in bytes"""
